66949e43485fc3e3f883cfc124d8b0b14684234d
Adrian Buchnat <adrianbuchnat@gmail.com>
2026-02-09 16:55:19 +0100

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,76 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+
+namespace GameEngine
+{
+    public class Game : GameWindow
+    {
+        int VertexBufferObject;
+
+        float[] vertices =
+        {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+
+        public Game(int width, int height, string title) : // To jest konstruktor
+           base(GameWindowSettings.Default, new NativeWindowSettings() {Size = (width, height), Title = title })
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e) // to jest metoda
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape)) { Close(); }
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            
+            VertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+        }
+    
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using GameEngine;
+
+Game game = new Game(1200, 800, "Witamy nowe okienko");
+
+game.Run();
